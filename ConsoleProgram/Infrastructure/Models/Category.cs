affd86bb97fc990d5bb08fd12a3a7c4d98684901
Naz Mammadova <mammadovanaz@gmail.com>
2022-08-20 21:03:39 +0400

    First Commit

---
+++ ConsoleProgram/Infrastructure/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace ConsoleProgram.Infrastructure.Models
+{
+    public class Category
+    {
+        static int count = 1;
+        public Category()
+        {
+            this.Id = count++;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+}
---
+++ ConsoleProgram/Infrastructure/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleProgram.Infrastructure.Models;
+using System;
+
+
+namespace ConsoleProgram.Infrastructure.Services
+{
+    public class Product
+    {
+        static int count = 1;
+        public Product()
+        {
+            Id = count++;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Quantity { get; set; }
+        public DateTime Date { get; set; }
+        public Category Category { get; set; }
+
+
+
+    }
+}
